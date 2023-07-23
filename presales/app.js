395ead9abbc6c82a3b7b16c25eb21735d95c4c37
iChris <christianjoseph473@gmail.com>
2023-07-23 10:53:26 +0100

    remove cookies from presales

--- presales/app.js
+++ presales/app.js
@@ -10,21 +10,3 @@ window.addEventListener('load', function() {
     pageContent.style.display = '';
     AOS.init();
 });
-
-const cookieBtn = document.querySelector('.cookie-btn');
-const modal = document.querySelector('.modal');
-
-const closeCookieModal = () => {
-    modal.style.display = 'none';
-    document.querySelector('.body').classList.remove('cookie-modal');
-    document.querySelector('.cookie-cont').style.display = 'none';
-    window.localStorage.setItem('cookie', 'true');
-}
-
-cookieBtn.addEventListener('click', closeCookieModal);
-
-const cookie = window.localStorage.getItem('cookie');
-
-if(cookie){
-    closeCookieModal();
-}
